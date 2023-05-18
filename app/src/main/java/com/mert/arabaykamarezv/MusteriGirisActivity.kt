b9ccab330a7cee22286863310d7f220f189836af
Batuhan Bardakkaya <batuhanbardakkaya@hotmail.com>
2023-05-18 17:05:58 +0300

    müşteri giriş kısmı yapıldı müşteri jobb sayfası eklendi

--- app/src/main/java/com/mert/arabaykamarezv/MusteriGirisActivity.kt
+++ app/src/main/java/com/mert/arabaykamarezv/MusteriGirisActivity.kt
@@ -1,10 +1,17 @@
 package com.mert.arabaykamarezv
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mert.arabaykamarezv.databinding.ActivityMusteriGirisBinding
 
 class MusteriGirisActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMusteriGirisBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +19,46 @@ class MusteriGirisActivity : AppCompatActivity() {
         binding = ActivityMusteriGirisBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        auth = Firebase.auth
+    }
+
+    fun Musterisignin(view: View) {
+        println("Onayy")
+        val eMail = binding.txtMusteriMail.text.toString()
+        val password = binding.txtMusteriPassword.text.toString()
+        if (eMail.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Bos Alan Bırakma", Toast.LENGTH_LONG).show()
+            println("Yanlıs")
+        } else {
+            println("Doğru")
+            auth.signInWithEmailAndPassword(eMail, password).addOnSuccessListener {
+
+                val intent = Intent(this, MusteriJobAddActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun Musterisignup(view: View) {
+        println("Onayy")
+        val eMail = binding.txtMusteriMail.text.toString()
+        val password = binding.txtMusteriPassword.text.toString()
+        if (eMail.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Bos Alan Bırakma", Toast.LENGTH_LONG).show()
+            println("Yanlıs")
+        } else {
+            println("Doğru")
+            auth.createUserWithEmailAndPassword(eMail, password).addOnSuccessListener {
+                println("oldu")
+                val intent = Intent(this, MusteriJobAddActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mert/arabaykamarezv/MusteriJobAddActivity.kt
@@ -0,0 +1,11 @@
+package com.mert.arabaykamarezv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MusteriJobAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_musteri_job_add)
+    }
+}
